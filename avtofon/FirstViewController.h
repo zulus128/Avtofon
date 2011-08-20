2f9792c60affaa84646d785ba194fb16e83b0e27
Zulus <git@smartapps.co>
2011-08-20 21:17:15 +0600

    Initial Commit

---
+++ avtofon/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  avtofon
+//
+//  Created by вадим on 8/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ avtofon/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  avtofon
+//
+//  Created by вадим on 8/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
