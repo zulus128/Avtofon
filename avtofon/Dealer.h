fa188be0ab6edadd83a57c8668c15a3857c2c09b
Zulus <git@smartapps.co>
2011-08-23 07:12:08 +0600

    3

---
+++ avtofon/Dealer.h
@@ -0,0 +1,13 @@
+//
+//  Dealer.h
+//  avtofon
+//
+//  Created by вадим on 8/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dealer : NSObject
+
+@end
---
+++ avtofon/Dealer.m
@@ -0,0 +1,23 @@
+//
+//  Dealer.m
+//  avtofon
+//
+//  Created by вадим on 8/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Dealer.h"
+
+@implementation Dealer
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
