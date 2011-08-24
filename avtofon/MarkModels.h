a23876b2ec8bfaac8ddc19de3030520f34bd4cae
Zulus <git@smartapps.co>
2011-08-24 07:30:52 +0600

    338

---
+++ avtofon/MarkModels.h
@@ -0,0 +1,13 @@
+//
+//  MarkModels.h
+//  avtofon
+//
+//  Created by вадим on 8/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MarkModels : UITableViewController
+
+@end
